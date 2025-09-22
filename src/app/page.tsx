0f31c1b443218210e6c06f12cf4187dba0c4ea63
mkmahmud <mahmudulhasanmk434@gmail.com>
2025-09-22 12:39:47 +0600

    update env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       }
 
       {
-        session?.user?.email && <div> <button onClick={() => signOut(session?.user?.email)}>Sign Out</button></div>
+        session?.user?.email && <div> <button onClick={() => signOut()}>Sign Out</button></div>
       }
 
 
